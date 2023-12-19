9db6bc22129f62c4019b25bb8f101246c849baca
liataynat <1345334884@qq.com>
2023-12-19 13:51:43 +0800

    end

---
+++ authors.py
@@ -0,0 +1,84 @@
+# 这是一个示例 Python 脚本。
+import pandas as pd
+import os
+import json
+import gzip
+
+# 按装订区域中的绿色按钮以运行脚本。
+if __name__ == '__main__':
+    current_dir = os.getcwd()
+    test_au_dir = os.path.join(current_dir, "authors")
+
+    for root, dirs, files in os.walk(test_au_dir):
+        for file_name in files:
+            directory_name = []
+            # 检查文件是否是 .gz 压缩文件
+            if file_name.endswith('.gz'):
+                file_path = os.path.join(root, file_name)
+                print(file_path)
+                # 创建解压后的文件路径
+                extract_file_path = os.path.splitext(file_path)[0]
+
+                # 解压缩文件
+                with gzip.open(file_path, 'rb') as gz_file:
+                    with open(extract_file_path, 'wb') as extracted_file:
+                        extracted_file.write(gz_file.read())
+
+                for i in range(3):
+                    # new_data.append(a_data)
+                    directory_name.append(os.path.basename(file_path))
+                    # print(directory_name)
+                    # 获取目录部分的路径
+                    file_path = os.path.dirname(file_path)
+
+            print(file_name)
+            file_name = file_name[:-3]
+
+            file_path = os.path.join(root, file_name)
+
+            # file_list = os.listdir(file_name)
+            # print(file_list)
+            # file_path = os.path.join(root, file_name)
+            # print(file_path)
+            with open(file_path, 'r') as file:
+                content = file.read()
+
+            num = 0
+            content = '[' + content + ']'
+            for char in content:
+                if char == "\n":
+                    num += 1
+
+            content = content.replace("}\n{", "},{")
+            data = json.loads(content)
+            # 转换数据格式
+            new_data = []
+            for index in range(num):
+                a_data = {
+                    "id": data[index].get("id"),
+                    "display_name": data[index].get("display_name", "NULL"),
+                    "cited_by_count": data[index].get("cited_by_count", "NULL"),
+                    "counts_by_year": data[index].get("counts_by_year", "NULL"),
+                    "works_count": data[index].get("works_count", "NULL"),
+                    "most_cited_work": data[index].get("most_cited_work", "NULL"),
+                    "last_known_institution": data[index].get("last_known_institution", "NULL"),
+                    "summary_stats": data[index].get("summary_stats", "NULL"),
+                    "works_api_url": data[index].get("works_api_url", "NULL"),
+                    # "display_name": data[index]["display_name"]
+                }
+                new_data.append(a_data)
+
+            file_name = file_name+".json"
+            # 输出到 data.json 文件
+            file_name = directory_name[2]+"_"+directory_name[1]+"_"+file_name
+            with open(file_name, 'w') as file:
+                file.write('[')
+                # json.dump(new_data, file, indent=4)
+                for index in range(num - 1):
+                    json.dump(new_data[index], file, indent=4)
+                    file.write(',')
+                json.dump(new_data[num - 1], file, indent=4)
+                file.write(']')
+
+            # 此时在当前目录下有相应的json文件,需上传至服务器
+
---
+++ works.py
@@ -0,0 +1,95 @@
+# 这是一个示例 Python 脚本。
+import pandas as pd
+import os
+import json
+import gzip
+
+# 按装订区域中的绿色按钮以运行脚本。
+if __name__ == '__main__':
+    current_dir = os.getcwd()
+    test_au_dir = os.path.join(current_dir, "works")
+
+    for root, dirs, files in os.walk(test_au_dir):
+        for file_name in files:
+            directory_name = []
+            # 检查文件是否是 .gz 压缩文件
+            if file_name.endswith('.gz'):
+                file_path = os.path.join(root, file_name)
+                print(file_path)
+                # 创建解压后的文件路径
+                extract_file_path = os.path.splitext(file_path)[0]
+
+                # 解压缩文件
+                with gzip.open(file_path, 'rb') as gz_file:
+                    with open(extract_file_path, 'wb') as extracted_file:
+                        extracted_file.write(gz_file.read())
+
+                for i in range(3):
+                    # new_data.append(a_data)
+                    directory_name.append(os.path.basename(file_path))
+                    # print(directory_name)
+                    # 获取目录部分的路径
+                    file_path = os.path.dirname(file_path)
+
+            print(file_name)
+            file_name = file_name[:-3]
+
+            file_path = os.path.join(root, file_name)
+
+            # file_list = os.listdir(file_name)
+            # print(file_list)
+            # file_path = os.path.join(root, file_name)
+            # print(file_path)
+            with open(file_path, 'r') as file:
+                content = file.read()
+
+            num = 0
+            content = '[' + content + ']'
+            for char in content:
+                if char == "\n":
+                    num += 1
+
+            content = content.replace("}\n{", "},{")
+            data = json.loads(content)
+            # 转换数据格式
+            new_data = []
+            for index in range(num):
+                a_data = {
+                        "id": data[index].get("id", "NULL"),
+                        "doi": data[index].get("doi", "NULL"),
+                        "title": data[index].get("title", "NULL"),
+                        "display_name": data[index].get("display_name", "NULL"),
+                        "publication_year": data[index].get("publication_year", "NULL"),
+                        "publication_date": data[index].get("publication_date", "NULL"),
+                        "language": data[index].get("language", "NULL"),
+                        "primary_location": data[index].get("primary_location", "NULL"),
+                        "type": data[index].get("type", "NULL"),
+                        "authorships": data[index].get("authorships", "NULL"),
+                        "countries_distinct_count": data[index].get("countries_distinct_count", "NULL"),
+                        "institutions_distinct_count": data[index].get("institutions_distinct_count", "NULL"),
+                        "cited_by_count": data[index].get("cited_by_count", "NULL"),
+                        "keywords": data[index].get("keywords", "NULL"),  # 设置默认值为 "NULL"
+                        "referenced_works_count": data[index].get("referenced_works_count", "NULL"),
+                        "referenced_works": data[index].get("referenced_works", "NULL"),
+                        "related_works": data[index].get("related_works", "NULL"),
+                        "counts_by_year": data[index].get("counts_by_year", "NULL"),
+                        "updated_date": data[index].get("updated_date", "NULL"),
+                        "created_date": data[index].get("created_date", "NULL"),
+                    # "display_name": data[index]["display_name"]
+                }
+                new_data.append(a_data)
+
+            file_name = file_name+".json"
+            # 输出到 data.json 文件
+            file_name = directory_name[2]+"_"+directory_name[1]+"_"+file_name
+            with open(file_name, 'w') as file:
+                file.write('[')
+                # json.dump(new_data, file, indent=4)
+                for index in range(num - 1):
+                    json.dump(new_data[index], file, indent=4)
+                    file.write(',')
+                json.dump(new_data[num - 1], file, indent=4)
+                file.write(']')
+
+            # 此时在当前目录下有相应的json文件,需上传至服务器
+
