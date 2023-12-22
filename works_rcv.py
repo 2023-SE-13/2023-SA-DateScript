ef42731b47b2172a251cdb945e7054bbc0dc6bb4
QAQLeonard <leonardzhou2077@gmail.com>
2023-12-22 08:20:39 +0800

    downgrade

--- works_rcv.py
+++ works_rcv.py
@@ -2,7 +2,6 @@ import os
 import time
 import json
 import gzip
-import threading
 from elasticsearch import Elasticsearch, helpers
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
@@ -13,6 +12,7 @@ ES_PORT = 9200
 ES_USERNAME = "elastic"  # 替换为您的 Elasticsearch 用户名
 ES_PASSWORD = "yXC0ZTAbjmhmyLHb7fBv"  # 替换为您的 Elasticsearch 密码
 INDEX_NAME = "works"         # 根据您的需求更改
+DOC_TYPE = "_doc"
 
 # 初始化 Elasticsearch 客户端
 es = Elasticsearch(
@@ -33,7 +33,6 @@ def decompress_gz(gz_path, output_path):
 
 # 准备批量上传的函数
 def bulk_index(file_path):
-    temp_val = 0
     try:
         with open(file_path, 'r') as file:
             actions = []
@@ -52,7 +51,6 @@ def bulk_index(file_path):
                 if count % 100 == 0:
                     helpers.bulk(es, actions)
                     # print("11111")
-                    temp_val = temp_val + 1
                     actions = []  # 清空列表以便下一批数据
 
             # 处理剩余的数据（如果有）
@@ -61,7 +59,7 @@ def bulk_index(file_path):
 
         return True
     except Exception as e:
-        print(f"Error indexing file {file_path} {temp_val*100}: {e}")
+        print(f"Error indexing file {file_path}: {e}")
         return False
 
 
@@ -87,27 +85,6 @@ class Watcher:
         self.observer.join()
 
 class Handler(FileSystemEventHandler):
-    @staticmethod
-    def handle_file(file_path, is_gzipped, done_file_path):
-        if is_gzipped:
-            json_file_path = file_path[:-3]  # 移除 ".gz" 后缀以获得JSON文件路径
-            print(f"Begin to decompress file: {json_file_path}")
-            decompress_gz(file_path, json_file_path)
-            print(f"Decompress file done: {json_file_path}")
-        else:
-            json_file_path = file_path
-
-        print(f"Begin to index JSON file: {json_file_path}")
-        if bulk_index(json_file_path):
-            os.remove(json_file_path)
-            print(f"Finished indexing and deleted JSON file: {json_file_path}")
-        else:
-            print(f"Failed to index JSON file: {json_file_path}")
-        if is_gzipped:
-            # 删除原始的gzip文件
-            os.remove(file_path)
-        os.remove(done_file_path)
-
 
     @staticmethod
     def on_created(event):
@@ -115,21 +92,48 @@ class Handler(FileSystemEventHandler):
             return None
 
         if event.event_type == 'created' and event.src_path.endswith('.done'):
+            # 获取JSON文件的路径
             json_file_path = event.src_path[:-5]
             print(f"Detected .done file, processing: {json_file_path}")
-            
-            # 创建一个线程来处理JSON文件
-            threading.Thread(target=Handler.handle_file, args=(json_file_path, False, event.src_path)).start()
 
+            if os.path.exists(json_file_path):
+                if bulk_index(json_file_path):
+                    os.remove(json_file_path)
+                    print(f"Finished indexing and deleted file: {json_file_path}")
+                else:
+                    print(f"Failed to index file: {json_file_path}")
+            else:
+                print("error error error")
 
+            # 删除.done文件
+            os.remove(event.src_path)
+        
         elif event.event_type == 'created' and event.src_path.endswith('.gzdone'):
+            # 获取gzip文件的路径
             gzip_file_path = event.src_path[:-7]  # 移除 ".gzdone" 后缀
+            json_file_path = gzip_file_path[:-3]  # 移除 ".gz" 后缀以获得JSON文件路径
 
             print(f"Detected .gzdone file, processing: {gzip_file_path}")
 
-            # 创建一个线程来处理gzip文件
-            threading.Thread(target=Handler.handle_file, args=(gzip_file_path, True, event.src_path)).start()
+            # 解压gzip文件
+            if os.path.exists(gzip_file_path):
+                decompress_gz(gzip_file_path, json_file_path)
+                print(f"Decompressed file: {json_file_path}")
 
+                # 索引解压后的JSON文件
+                if bulk_index(json_file_path):
+                    os.remove(json_file_path)
+                    print(f"Finished indexing and deleted JSON file: {json_file_path}")
+                else:
+                    print(f"Failed to index JSON file: {json_file_path}")
+
+                # 删除原始的gzip文件
+                os.remove(gzip_file_path)
+            else:
+                print("Gzip file not found")
+
+            # 删除.gzdone文件
+            os.remove(event.src_path)
 
 
 if __name__ == '__main__':
